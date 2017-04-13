689b30cb07bb11fe066c657324b1de96a7415a53
Latikaa <latikaa@policybazaar.com>
2017-04-13 13:55:20 +0530

    change server name

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,25 @@
 'use strict';
 
 require('./generator-runtime');
+
 module.exports = require('./json-rules-engine');
+module.exports = require('../src/chatbot-rules/common_rules');
+
+var express = require('express');
+var app = express();
+
+app.get('/rules', function (req, res) {
+    var anc = module.exports.callRules();
+    console.log("anc is" + anc);
+    res.end(anc);
+});
+
+var server = app.listen(8081, function () {
+
+  //var host = server.address().address;
+  //var port = server.address().port;
+    var host = "localhost";
+    var port = 8081;
+    console.log("Example app listening at http://%s:%s", host, port);
+
+});
